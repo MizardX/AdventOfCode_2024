57d18bbec5fab16550af1dd1a85054b34cadcc3b
Markus Jarderot <markus.jarderot.ext@lkab.com>
2024-12-12 11:33:40 +0100

    Day 12 - Solved part 1 & 2

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,3 +1,4 @@
+use std::fmt::{self, Display, Formatter};
 use std::str::FromStr;
 use thiserror::Error;
 
@@ -6,6 +7,8 @@ use crate::aoclib::Grid;
 const EXAMPLE1: &str = include_str!("example1.txt");
 const EXAMPLE2: &str = include_str!("example2.txt");
 const EXAMPLE3: &str = include_str!("example3.txt");
+const EXAMPLE4: &str = include_str!("example4.txt");
+const EXAMPLE5: &str = include_str!("example5.txt");
 const INPUT: &str = include_str!("input.txt");
 
 pub fn run() {
@@ -14,22 +17,32 @@ pub fn run() {
     println!("++Example 1");
     let example1 = EXAMPLE1.parse().expect("Parse example 1");
     println!("|+-Part 1: {} (expected 140)", part_1(&example1));
-    // println!("|'-Part 2: {} (expected XXX)", part_2(&example1));
+    println!("|'-Part 2: {} (expected 80)", part_2(&example1));
 
     println!("++Example 2");
     let example2 = EXAMPLE2.parse().expect("Parse example 2");
     println!("|+-Part 1: {} (expected 772)", part_1(&example2));
-    // println!("|'-Part 2: {} (expected XXX)", part_2(&example2));
+    println!("|'-Part 2: {} (expected 436)", part_2(&example2));
 
     println!("++Example 3");
     let example3 = EXAMPLE3.parse().expect("Parse example 3");
-    println!("|+-Part 1: {} (expected 1930)", part_1(&example3));
-    // println!("|'-Part 2: {} (expected XXX)", part_2(&example3));
+    println!("|+-Part 1: {} (expected 1_930)", part_1(&example3));
+    println!("|'-Part 2: {} (expected 1_206)", part_2(&example3));
+
+    println!("++Example 4");
+    let example4 = EXAMPLE4.parse().expect("Parse example 4");
+    println!("|+-Part 1: {} (expected 692)", part_1(&example4));
+    println!("|+-Part 2: {} (expected 236)", part_2(&example4));
+
+    println!("++Example 5");
+    let example5 = EXAMPLE5.parse().expect("Parse example 5");
+    println!("|+-Part 1: {} (expected 1_184)", part_1(&example5));
+    println!("|+-Part 2: {} (expected 368)", part_2(&example5));
 
     println!("++Input");
     let input = INPUT.parse().expect("Parse input");
-    println!("|+-Part 1: {} (expected XXX)", part_1(&input));
-    println!("|'-Part 2: {} (expected XXX)", part_2(&input));
+    println!("|+-Part 1: {} (expected 1_573_474)", part_1(&input));
+    println!("|'-Part 2: {} (expected 966_476)", part_2(&input));
     println!("')");
 }
 
@@ -57,7 +70,92 @@ pub fn part_1(input: &Input) -> usize {
     for r in 0..input.plots.height() {
         for c in 0..input.plots.width() {
             let id = r * input.plots.width() + c;
-            if let Some(size) = ds.size(id) {
+            let root = ds.find(id);
+            if let Some(size) = ds.size(root) {
+                total_cost += size * perimiter[id];
+            }
+        }
+    }
+    total_cost
+}
+
+#[must_use]
+pub fn part_2(input: &Input) -> usize {
+    let mut ds = DisjointSet::new(input.plots.height() * input.plots.width());
+    let stride = input.plots.width();
+    let mut perimiter = vec![4; input.plots.height() * input.plots.width()];
+    for (r, row) in input.plots.rows().enumerate() {
+        for (c, &plot) in row.iter().enumerate() {
+            let id = r * input.plots.width() + c;
+            if c == 0 && r > 0 && input.plots.get(c, r - 1) == Some(&plot) {
+                // first column, same as above, so left fence continues. self--
+                perimiter[id] -= 1;
+            }
+            if c == input.plots.width() - 1 && r > 0 && input.plots.get(c, r - 1) == Some(&plot) {
+                // last column, same as above, so right fence continues. self--
+                perimiter[id] -= 1;
+            }
+            if c > 0 && row[c - 1] == plot {
+                // same as left, so left fence does not exist, and right fence of left plot does not exist. self--, left--
+                ds.union(id, id - 1);
+                perimiter[id] -= 1;
+                perimiter[id - 1] -= 1;
+            }
+            if r == 0 && c > 0 && input.plots.get(c - 1, r) == Some(&plot) {
+                // first row, same as left, so above fence continues. self--
+                perimiter[id] -= 1;
+            }
+            if r == input.plots.height() - 1 && c > 0 && input.plots.get(c - 1, r) == Some(&plot) {
+                // last row, same as left, so below fence continues. self--
+                perimiter[id] -= 1;
+            }
+            if r > 0 && input.plots.get(c, r - 1) == Some(&plot) {
+                // same as above, so above fence does not exist, and below fence of above plot does not exist. self--, above--
+                ds.union(id, id - stride);
+                perimiter[id] -= 1;
+                perimiter[id - stride] -= 1;
+            }
+            if r > 0 && c > 0 {
+                if let (Some(&diag), Some(&left), Some(&above)) = (
+                    input.plots.get(c - 1, r - 1),
+                    input.plots.get(c - 1, r),
+                    input.plots.get(c, r - 1),
+                ) {
+                    // quad case {diag} {above} / {left} {plot}
+                    if above == plot && left != plot && diag != plot {
+                        // left fence countinues. self--
+                        // .X
+                        // .X
+                        perimiter[id] -= 1;
+                    }
+                    if left != plot && above != diag && diag == left {
+                        // right fence of left region countinues. left--
+                        // Y.
+                        // Y.
+                        perimiter[id - 1] -= 1;
+                    }
+                    if above != plot && left == plot && diag != plot {
+                        // above fence countinues. self--
+                        // ..
+                        // XX
+                        perimiter[id] -= 1;
+                    }
+                    if left != diag && above != plot && diag == above {
+                        // below fence of above region countinues. above--
+                        // YY
+                        // ..
+                        perimiter[id - stride] -= 1;
+                    }
+                }
+            }
+        }
+    }
+    let mut total_cost = 0;
+    for r in 0..input.plots.height() {
+        for c in 0..input.plots.width() {
+            let id = r * input.plots.width() + c;
+            let root = ds.find(id);
+            if let Some(size) = ds.size(root) {
                 total_cost += size * perimiter[id];
             }
         }
@@ -96,25 +194,17 @@ impl DisjointSet {
     }
 
     fn union(&mut self, a: usize, b: usize) {
-        let a = self.find(a);
-        let b = self.find(b);
+        let mut a = self.find(a);
+        let mut b = self.find(b);
         if a == b {
             return;
         }
         if self.sizes[a] < self.sizes[b] {
-            self.parents[a] = b;
-            self.sizes[b] += self.sizes[a];
-        } else {
-            self.parents[b] = a;
-            self.sizes[a] += self.sizes[b];
+            (a, b) = (b, a);
         }
+        self.parents[b] = a;
+        self.sizes[a] += self.sizes[b];
     }
-}
-
-#[must_use]
-pub fn part_2(input: &Input) -> usize {
-    let _ = input;
-    0
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -125,6 +215,12 @@ impl TryFrom<u8> for Plot {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         Ok(Self(value))
+    }
+}
+
+impl Display for Plot {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}", self.0 as char)
     }
 }
 
