f2af07d2557698a28622f464feb6e9916ddf446f
Markus Jarderot <mizardx@gmail.com>
2024-12-31 17:15:30 +0100

    Day 24 - Refactor

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, VecDeque};
+use std::str::FromStr;
 use thiserror::Error;
 
 const EXAMPLE1: &str = include_str!("example1.txt");
@@ -11,12 +12,10 @@ pub fn run() {
     println!("++Example 1");
     let example1 = EXAMPLE1.try_into().expect("Parse example");
     println!("|+-Part 1: {} (expected 4)", part_1(&example1));
-    // println!("|'-Part 2: {} (expected XXX)", part_2(&example1));
 
     println!("++Example 2");
     let example2 = EXAMPLE2.try_into().expect("Parse example");
     println!("|+-Part 1: {} (expected 2_024)", part_1(&example2));
-    // println!("|'-Part 2: {} (expected XXX)", part_2(&example2));
 
     println!("++Input");
     let input = INPUT.try_into().expect("Parse input");
@@ -32,131 +31,86 @@ pub fn run() {
 }
 
 #[must_use]
-pub fn part_1(input: &Input) -> u64 {
-    let n = input.gates.len();
-    let mut values = vec![None; n];
-    run_circuit(&input.gates, &mut values);
-    extract_output(input, &values).unwrap()
-}
-
-fn run_circuit(gates: &[Gate<'_>], values: &mut [Option<bool>]) {
-    let n = gates.len();
-    let mut waiting_on = vec![vec![]; n];
-    let mut pending: VecDeque<_> = (0..n).collect();
-    let mut queries = 0;
-    while let Some(index) = pending.pop_front() {
-        if values[index].is_none() {
-            queries += 1;
-            let gate = &gates[index];
-            match gate.kind.evaluate(values) {
-                Ok(value) => {
-                    values[index] = Some(value);
-                    for next in waiting_on[index].drain(..) {
-                        pending.push_back(next);
-                    }
-                }
-                Err(other) => waiting_on[other].push(index),
-            }
-        }
-    }
-    println!("Queries: {queries}");
-}
-
-fn extract_output(input: &Input<'_>, values: &[Option<bool>]) -> Option<u64> {
-    let mut output = 0;
-    for (shift, &index) in input.output_gates.iter().enumerate() {
-        let value: bool = values[index]?;
-        output |= u64::from(value) << shift;
-    }
-    Some(output)
+pub fn part_1(input: &Circuit) -> u64 {
+    let mut runner = CircuitRunner::new(input);
+    runner.run();
+    runner.get_output().unwrap()
 }
 
 #[must_use]
-pub fn part_2(input: &Input) -> String {
-    let mut wrongs = Vec::new();
+pub fn part_2(input: &Circuit) -> String {
+    let wrongs = input.validate_circuit();
 
-    // The XOR directly connected to the input. The partial sum.
-    let mut input_adds = Vec::new();
-    // The AND directly connected to the input. A partial carry.
-    let mut input_carries = Vec::new();
-    // The XOR that is the output. The full sum.
-    let mut full_adds = Vec::new();
-    // The AND that is connected to the incoming carry. A partial carry, or "carry-carry".
-    let mut inner_carry = Vec::new();
-    // The OR that combines the full carries. The final carry.
-    let mut full_carries = Vec::new();
+    assert_eq!(
+        wrongs.len(),
+        8,
+        "Expected 8 wrong gates that where missplaced, found {}",
+        wrongs.len()
+    );
 
-    for (index, gate) in input.gates.iter().enumerate() {
-        if let GateKind::Binary(bin_op, lhs, rhs) = gate.kind {
-            let is_input = input.gates[lhs].kind.is_constant() || input.gates[rhs].kind.is_constant();
-            match (bin_op, is_input) {
-                (BinOp::And, true) => input_carries.push(index),
-                (BinOp::And, false) => inner_carry.push(index),
-                (BinOp::Or, true) => panic!("Inputs should not have been swapped to an OR gate"),
-                (BinOp::Or, false) => full_carries.push(index),
-                (BinOp::Xor, true) => input_adds.push(index),
-                (BinOp::Xor, false) => full_adds.push(index),
+    let mut names: Vec<_> = wrongs
+        .into_iter()
+        .map(|gate_index| input.gates[gate_index].name)
+        .collect();
+    names.sort_unstable();
+    names.join(",")
+}
+
+#[derive(Debug, Clone)]
+struct CircuitRunner<'a> {
+    circuit: &'a Circuit<'a>,
+    values: Vec<Option<bool>>,
+}
+
+impl<'a> CircuitRunner<'a> {
+    fn new(circuit: &'a Circuit<'a>) -> Self {
+        let n = circuit.gates.len();
+        let values = vec![None; n];
+        Self { circuit, values }
+    }
+
+    fn run(&mut self) {
+        let n = self.values.len();
+        let mut waiting_on = vec![vec![]; n];
+        let mut pending: VecDeque<_> = (0..n).collect();
+        while let Some(gate_index) = pending.pop_front() {
+            if self.values[gate_index].is_none() {
+                match self.evaluate_gate(gate_index) {
+                    Ok(value) => {
+                        self.values[gate_index] = Some(value);
+                        for next in waiting_on[gate_index].drain(..) {
+                            pending.push_back(next);
+                        }
+                    }
+                    Err(missing) => waiting_on[missing].push(gate_index),
+                }
             }
         }
     }
 
-    let first_input1 = input.input_gates1.first().copied().unwrap();
-    let last_output = input.output_gates.last().copied().unwrap();
-
-    // If the XOR gate either doesn't show up in other XOR operations, or is an output, then it is wrong.
-    for &gate in &input_adds {
-        let has_first_input_operand = input.gates[gate].kind.has_operand(first_input1);
-        let is_not_output_gate = !input.output_gates.contains(&gate);
-        let is_not_in_full_adds = full_adds.iter().all(|&a| !input.gates[a].kind.has_operand(gate));
-
-        if !has_first_input_operand && is_not_output_gate && is_not_in_full_adds {
-            wrongs.push(gate);
-        }
+    fn evaluate_gate(&self, gate: usize) -> Result<bool, usize> {
+        Ok(match self.circuit.gates[gate].kind {
+            GateKind::None => unreachable!(),
+            GateKind::Constant(value) => value,
+            GateKind::Binary(binop, lhs, rhs) => {
+                let lhs_value = self.values[lhs].ok_or(lhs)?;
+                let rhs_value = self.values[rhs].ok_or(rhs)?;
+                match binop {
+                    BinOp::And => lhs_value && rhs_value,
+                    BinOp::Or => lhs_value || rhs_value,
+                    BinOp::Xor => lhs_value ^ rhs_value,
+                }
+            }
+        })
     }
 
-    // If the AND gate either doesn't show up in full carries, or is an output, then it is wrong.
-    for &gate in &input_carries {
-        let has_first_input_operand = input.gates[gate].kind.has_operand(first_input1);
-        let is_output_gate = input.output_gates.contains(&gate);
-        let is_not_in_full_carries = full_carries
-            .iter()
-            .all(|&a| !input.gates[a].kind.has_operand(gate));
-
-        if !has_first_input_operand && (is_output_gate || is_not_in_full_carries) {
-            wrongs.push(gate);
+    fn get_output(&self) -> Option<u64> {
+        let mut output = 0;
+        for (shift, &index) in self.circuit.output_gates.iter().enumerate() {
+            output |= u64::from(self.values[index]?) << shift;
         }
+        Some(output)
     }
-
-    // If the XOR gate isn't an output, then it is wrong
-    for &gate in &full_adds {
-        let is_output_gate = input.output_gates.contains(&gate);
-        if !is_output_gate {
-            wrongs.push(gate);
-        }
-    }
-
-    // If the OR gate is an output, except for the last output, which doubles as a carry, then it is wrong.
-    for &gate in &full_carries {
-        let is_output_gate = input.output_gates.contains(&gate);
-        let is_last_output_gate = gate == last_output;
-        if is_output_gate && !is_last_output_gate {
-            wrongs.push(gate);
-        }
-    }
-
-    // If the AND gate is an output, then it is wrong.
-    for &gate in &inner_carry {
-        let is_output_gate = input.output_gates.contains(&gate);
-        if is_output_gate {
-            wrongs.push(gate);
-        }
-    }
-
-    assert_eq!(wrongs.len(), 8, "Expected 8 wrong gates that where missplaced, found {}", wrongs.len());
-
-    let mut names: Vec<_> = wrongs.into_iter().map(|s| input.gates[s].name).collect();
-    names.sort_unstable();
-    names.join(",")
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -164,6 +118,19 @@ enum BinOp {
     And,
     Or,
     Xor,
+}
+
+impl FromStr for BinOp {
+    type Err = ParseInputError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "AND" => Ok(Self::And),
+            "OR" => Ok(Self::Or),
+            "XOR" => Ok(Self::Xor),
+            _ => Err(ParseInputError::SyntaxError),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
@@ -174,23 +141,9 @@ enum GateKind {
     Constant(bool),
 }
 
-#[expect(dead_code)]
 impl GateKind {
-    const fn is_or(self) -> bool {
-        matches!(self, Self::Binary(BinOp::Or, _, _))
-    }
-    const fn is_and(self) -> bool {
-        matches!(self, Self::Binary(BinOp::And, _, _))
-    }
-    const fn is_xor(self) -> bool {
-        matches!(self, Self::Binary(BinOp::Xor, _, _))
-    }
     const fn is_constant(self) -> bool {
         matches!(self, Self::Constant(_))
-    }
-
-    const fn is_binary(self) -> bool {
-        matches!(self, Self::Binary(_, _, _))
     }
 
     const fn has_operand(self, operand: usize) -> bool {
@@ -198,77 +151,6 @@ impl GateKind {
             Self::Binary(_, lhs, rhs) => lhs == operand || rhs == operand,
             _ => false,
         }
-    }
-
-    fn other_operand(self, first_operand: usize) -> Option<usize> {
-        let (lhs, rhs) = self.as_binary()?;
-        Some(if lhs == first_operand {
-            rhs
-        } else if rhs == first_operand {
-            lhs
-        } else {
-            None?
-        })
-    }
-
-    fn children(self) -> Option<[usize; 2]> {
-        self.as_binary().map(Into::into)
-    }
-
-    const fn as_binary(self) -> Option<(usize, usize)> {
-        match self {
-            Self::Binary(_, lhs, rhs) => Some((lhs, rhs)),
-            _ => None,
-        }
-    }
-    const fn as_and(self) -> Option<(usize, usize)> {
-        match self {
-            Self::Binary(BinOp::And, lhs, rhs) => Some((lhs, rhs)),
-            _ => None,
-        }
-    }
-    const fn as_or(self) -> Option<(usize, usize)> {
-        match self {
-            Self::Binary(BinOp::Or, lhs, rhs) => Some((lhs, rhs)),
-            _ => None,
-        }
-    }
-    const fn as_xor(self) -> Option<(usize, usize)> {
-        match self {
-            Self::Binary(BinOp::Xor, lhs, rhs) => Some((lhs, rhs)),
-            _ => None,
-        }
-    }
-
-    const fn flip(self) -> Option<Self> {
-        Some(match self {
-            Self::Binary(BinOp::And, lhs, rhs) => Self::Binary(BinOp::Or, lhs, rhs),
-            Self::Binary(BinOp::Or, lhs, rhs) => Self::Binary(BinOp::And, lhs, rhs),
-            Self::Binary(BinOp::Xor, lhs, rhs) => Self::Binary(BinOp::Xor, lhs, rhs),
-            _ => return None,
-        })
-    }
-
-    fn evaluate(self, inputs: &[Option<bool>]) -> Result<bool, usize> {
-        Ok(match self {
-            Self::None => unreachable!(),
-            Self::Constant(value) => value,
-            Self::Binary(BinOp::And, lhs, rhs) => {
-                let lhs_value = inputs[lhs].ok_or(lhs)?;
-                let rhs_value = inputs[rhs].ok_or(rhs)?;
-                lhs_value && rhs_value
-            }
-            Self::Binary(BinOp::Or, lhs, rhs) => {
-                let lhs_value = inputs[lhs].ok_or(lhs)?;
-                let rhs_value = inputs[rhs].ok_or(rhs)?;
-                lhs_value || rhs_value
-            }
-            Self::Binary(BinOp::Xor, lhs, rhs) => {
-                let lhs_value = inputs[lhs].ok_or(lhs)?;
-                let rhs_value = inputs[rhs].ok_or(rhs)?;
-                lhs_value ^ rhs_value
-            }
-        })
     }
 }
 
@@ -280,11 +162,112 @@ struct Gate<'a> {
 
 #[derive(Debug, Clone)]
 #[allow(unused)]
-pub struct Input<'a> {
+pub struct Circuit<'a> {
     gates: Vec<Gate<'a>>,
     input_gates1: Vec<usize>,
     input_gates2: Vec<usize>,
     output_gates: Vec<usize>,
+}
+
+impl Circuit<'_> {
+    fn validate_circuit(&self) -> Vec<usize> {
+        let mut wrongs = Vec::new();
+
+        // The XOR directly connected to the input. The partial sum.
+        let mut input_adds = Vec::new();
+        // The AND directly connected to the input. A partial carry.
+        let mut input_carries = Vec::new();
+        // The XOR that is the output. The full sum.
+        let mut full_adds = Vec::new();
+        // The AND that is connected to the incoming carry. A partial carry, or "carry-carry".
+        let mut inner_carries = Vec::new();
+        // The OR that combines the full carries. The final carry.
+        let mut full_carries = Vec::new();
+
+        for (index, gate) in self.gates.iter().enumerate() {
+            if let GateKind::Binary(bin_op, lhs, rhs) = gate.kind {
+                let is_input =
+                    self.gates[lhs].kind.is_constant() || self.gates[rhs].kind.is_constant();
+                match (bin_op, is_input) {
+                    (BinOp::And, true) => input_carries.push(index),
+                    (BinOp::And, false) => inner_carries.push(index),
+                    (BinOp::Or, true) => {
+                        panic!("Inputs should not have been swapped to an OR gate")
+                    }
+                    (BinOp::Or, false) => full_carries.push(index),
+                    (BinOp::Xor, true) => input_adds.push(index),
+                    (BinOp::Xor, false) => full_adds.push(index),
+                }
+            }
+        }
+
+        let first_input1 = self.input_gates1.first().copied().unwrap();
+        let last_output = self.output_gates.last().copied().unwrap();
+
+        // If the XOR gate either doesn't show up in other XOR operations, or is an output, then it is wrong.
+        for &gate in &input_adds {
+            let has_first_input_operand = self.gates[gate].kind.has_operand(first_input1);
+            let is_not_output_gate = !self.output_gates.contains(&gate);
+            let is_not_in_full_adds = full_adds
+                .iter()
+                .all(|&a| !self.gates[a].kind.has_operand(gate));
+
+            if !has_first_input_operand && is_not_output_gate && is_not_in_full_adds {
+                wrongs.push(gate);
+                // println!("input add XOR gate {} is wrong, has_first_input_operand={has_first_input_operand}, is_not_output_gate={is_not_output_gate}, is_not_in_full_adds={is_not_in_full_adds}", self.gates[gate].name);
+            }
+        }
+
+        // If the AND gate either doesn't show up in full carries, or is an output, then it is wrong.
+        for &gate in &input_carries {
+            let has_first_input_operand = self.gates[gate].kind.has_operand(first_input1);
+            let is_output_gate = self.output_gates.contains(&gate);
+            let is_not_in_full_carries = full_carries
+                .iter()
+                .all(|&a| !self.gates[a].kind.has_operand(gate));
+
+            if !has_first_input_operand && (is_output_gate || is_not_in_full_carries) {
+                wrongs.push(gate);
+                // println!("input carry AND gate {} is wrong, has_first_input_operand={has_first_input_operand}, is_output_gate={is_output_gate}, is_not_in_full_carries={is_not_in_full_carries}", self.gates[gate].name);
+            }
+        }
+
+        // If the XOR gate isn't an output, then it is wrong
+        for &gate in &full_adds {
+            let is_output_gate = self.output_gates.contains(&gate);
+            if !is_output_gate {
+                wrongs.push(gate);
+                // println!(
+                //     "full add XOR gate {} is wrong, is_output_gate={is_output_gate}",
+                //     self.gates[gate].name
+                // );
+            }
+        }
+
+        // If the OR gate is an output, except for the last output, which doubles as a carry, then it is wrong.
+        for &gate in &full_carries {
+            let is_output_gate = self.output_gates.contains(&gate);
+            let is_last_output_gate = gate == last_output;
+            if is_output_gate && !is_last_output_gate {
+                wrongs.push(gate);
+                // println!("full carry OR gate {} is wrong, is_output_gate={is_output_gate}, is_last_output_gate={is_last_output_gate}", self.gates[gate].name);
+            }
+        }
+
+        // If the AND gate is an output, then it is wrong.
+        for &gate in &inner_carries {
+            let is_output_gate = self.output_gates.contains(&gate);
+            if is_output_gate {
+                wrongs.push(gate);
+                // println!(
+                //     "inner carry AND gate {} is wrong, is_output_gate={is_output_gate}",
+                //     self.gates[gate].name
+                // );
+            }
+        }
+
+        wrongs
+    }
 }
 
 #[derive(Debug, Error)]
@@ -295,7 +278,7 @@ pub enum ParseInputError {
     SyntaxError,
 }
 
-impl<'a> TryFrom<&'a str> for Input<'a> {
+impl<'a> TryFrom<&'a str> for Circuit<'a> {
     type Error = ParseInputError;
 
     fn try_from(text: &'a str) -> Result<Self, Self::Error> {
@@ -307,7 +290,7 @@ impl<'a> TryFrom<&'a str> for Input<'a> {
         parse_gates(&mut gates, &mut lookup, lines)?;
         let (input_gates1, input_gates2, output_gates) = extract_inputs_and_outputs(&gates);
 
-        Ok(Input {
+        Ok(Circuit {
             gates,
             input_gates1,
             input_gates2,
@@ -321,7 +304,7 @@ fn parse_constants<'a>(
     lookup: &mut HashMap<&'a str, usize>,
     lines: &mut std::str::Lines<'a>,
 ) -> Result<(), ParseInputError> {
-    for line in lines.by_ref() {
+    for line in lines {
         if line.is_empty() {
             break;
         }
@@ -343,26 +326,17 @@ fn parse_gates<'a>(
     lines: std::str::Lines<'a>,
 ) -> Result<(), ParseInputError> {
     for line in lines {
-        let (lhs, rhs) = line
+        let (expr, output) = line
             .split_once(" -> ")
             .ok_or(ParseInputError::MissingDelimiter)?;
-        let kind = if let Some((left, right)) = lhs.split_once(" AND ") {
-            let left_gate = get_or_add_gate(gates, lookup, left);
-            let right_gate = get_or_add_gate(gates, lookup, right);
-            GateKind::Binary(BinOp::And, left_gate, right_gate)
-        } else if let Some((left, right)) = lhs.split_once(" OR ") {
-            let left_gate = get_or_add_gate(gates, lookup, left);
-            let right_gate = get_or_add_gate(gates, lookup, right);
-            GateKind::Binary(BinOp::Or, left_gate, right_gate)
-        } else if let Some((left, right)) = lhs.split_once(" XOR ") {
-            let left_gate = get_or_add_gate(gates, lookup, left);
-            let right_gate = get_or_add_gate(gates, lookup, right);
-            GateKind::Binary(BinOp::Xor, left_gate, right_gate)
-        } else {
-            return Err(ParseInputError::SyntaxError);
-        };
-        let gate = get_or_add_gate(gates, lookup, rhs);
-        gates[gate].kind = kind;
+        let (left, rest) = expr.split_once(' ').ok_or(ParseInputError::MissingDelimiter)?;
+        let (binop, right) = rest.split_once(' ').ok_or(ParseInputError::MissingDelimiter)?;
+
+        let left_gate = get_or_add_gate(gates, lookup, left);
+        let right_gate = get_or_add_gate(gates, lookup, right);
+        let output_gate = get_or_add_gate(gates, lookup, output);
+
+        gates[output_gate].kind = GateKind::Binary(binop.parse()?, left_gate, right_gate);
     }
     Ok(())
 }
